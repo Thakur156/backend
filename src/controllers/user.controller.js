534a6a12ac7a0e97e14b69a753c98bde3e69b2ce
thakur156 <aliisrar156@gmail.com>
2025-04-12 17:34:20 +0900

    return missing solved in jwt generation and .js ending error solved

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -110,7 +110,7 @@ const loginUser = asyncHandler(async (req, res) => {
 
   const { email, username, password } = req.body;
 
-  if (!username || !email) {
+  if (!(username || email)) {
     throw new ApiError(400, "username or email is required");
   }
 
@@ -132,7 +132,7 @@ const loginUser = asyncHandler(async (req, res) => {
     user._id
   );
 
-  const loggedInUser = User.findById(user._id).select(
+  const loggedInUser = await User.findById(user._id).select(
     "-password -refreshToken"
   );
 
