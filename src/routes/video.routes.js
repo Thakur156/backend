b75b550aa6b124a2016ffb4273e81b16f0284076
thakur156 <aliisrar156@gmail.com>
2025-04-15 00:00:06 +0900

    add video routes with JWT authentication and CRUD operations

---
+++ src/routes/video.routes.js
@@ -0,0 +1,31 @@
+import { Router } from "./user.routes";
+import {
+  getAllVideos,
+  publishAVideo,
+  getVideoById,
+  updateVideo,
+  deleteVideo,
+  togglePublishStatus,
+} from "../controllers/video.controller.js";
+
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
+const router = Router();
+
+router
+  .route("/")
+  .get(getAllVideos)
+  .post(
+    upload.fields([
+      {
+        name: "videoFile",
+        maxCount: 1,
+      },
+      {
+        name: "thumbnail",
+        maxCount: 1,
+      },
+    ]),
+    publishAVideo
+  );
