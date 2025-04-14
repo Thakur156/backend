b7f2fcadbad3f59ac4743ae51a4903306058394f
thakur156 <aliisrar156@gmail.com>
2025-04-15 00:28:00 +0900

    implement createTweet and getUserTweets functions with user validation

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -7,10 +7,33 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createTweet = asyncHandler(async (req, res) => {
   //TODO: create tweet
+
+  const { content } = req.body;
+  const userId = req.user?._id;
+  const user = await User.findById(userId);
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+  const tweet = await Tweet.create({
+    content,
+    owner: userId,
+  });
+
+  if (!tweet) {
+    throw new ApiError(500, "Failed to create tweet");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweet, "Tweet created successfully"));
 });
 
 const getUserTweets = asyncHandler(async (req, res) => {
   // TODO: get user tweets
+
+  const user = await User.findById(req.use?._id);
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
 });
 
 const updateTweet = asyncHandler(async (req, res) => {
